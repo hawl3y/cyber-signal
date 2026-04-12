3e3413273c4341c5e83d15f83fef7c88ac900652
hawl3y <joechawley@gmail.com>
2026-04-11 20:11:48 -0400

    Initial project setup: environment, Flask app, PostgreSQL, and structure

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from flask import Flask
+from config import Config
+from app.extensions import db, migrate
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    @app.route("/")
+    def home():
+        return {"status": "ok"}
+
+    return app
